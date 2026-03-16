dc911d588c6e5d9b3ee4d675d29e048a92d0b583
UrielSC24 <2022371107@uteq.edu.mx>
2026-03-15 19:35:46 -0600

    smell corregidos para sonarqube

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
-var express = require('express')
-var router = express.Router()
+const express = require('express')
+const router = express.Router()
 
 router.get('/health', (req, res) => {
   res.status(200).json({ status: 'OK', uptime: process.uptime() })
