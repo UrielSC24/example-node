51b8cb58e93863fb4a6566b8d0a7041407f776ca
UrielSC24 <2022371107@uteq.edu.mx>
2026-03-19 00:35:49 -0600

    fix: corregir code smells en orders.js para SonarQube

--- routes/orders.js
+++ routes/orders.js
@@ -3,8 +3,8 @@ const router = express.Router()
 
 router.get('/', (req, res) => {
   res.status(200).json([
-    { id: 1, customer: 'Leon S. Kennedy', total: 150.00, status: 'Completed' },
-    { id: 2, customer: 'Dante Sparda', total: 85.50, status: 'Pending' }
+    { id: 1, customer: 'Leon S. Kennedy', total: 150, status: 'Completed' },
+    { id: 2, customer: 'Dante Sparda', total: 85.5, status: 'Pending' }
   ])
 })
 
